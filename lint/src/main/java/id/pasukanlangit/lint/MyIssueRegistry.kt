8c4f0b8759111ab87a118519fef32ad4aaf346b9
paslang <anandadwiprayoga@192.168.1.13>
2021-07-07 12:50:27 +0700

    initial

---
+++ lint/src/main/java/id/pasukanlangit/lint/MyIssueRegistry.kt
@@ -0,0 +1,14 @@
+package id.pasukanlangit.lint
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.CURRENT_API
+import com.android.tools.lint.detector.api.Issue
+
+@Suppress("UnstableApiUsage")
+class MyIssueRegistry : IssueRegistry() {
+    override val issues: List<Issue>
+        get() = listOf(NamingPatternDetector.ISSUE_NAMING_PATTERN)
+
+    override val api: Int
+        get() = CURRENT_API
+}
